488513d58e10dd1c177a55886bb09a7e3099c39e
TigDang <gizetdinov.m.f@gmail.com>
2022-01-19 00:50:55 +0300

    add beta version of lines between lights

--- garland-script.js
+++ garland-script.js
@@ -1,15 +1,22 @@
 function placeGarlandOnSite() {
     let garlandContainer = document.createElement('div');
     garlandContainer.id = 'garland-container';
+    garlandContainer.className = 'garlandRope';
+
+    let lineContainer = document.createElement('ul');
+    lineContainer.className = 'garland-line';
 
     for (let i = 0; i < 50; i++) {
-        let tempOfLight = document.createElement('div')
+        let tempOfLight = document.createElement('div');
+
+        lineContainer.appendChild(document.createElement('li'));
+
         tempOfLight.className = 'garland-light';
         tempOfLight.id = 'garlandNumber' + i;
         garlandContainer.appendChild(tempOfLight);
     }
 
-    garlandContainer.className = 'garlandRope';
+    document.body.appendChild(lineContainer);
     document.body.appendChild(garlandContainer);
 }
 
